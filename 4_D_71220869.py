24536e2a93b01ff6ff3f65394a2c7ed492886de1
Nicholandn22 <117884361+Nicholandn22@users.noreply.github.com>
2023-03-01 18:58:38 +0700

    Add files via upload

--- 4_D_71220869.py
+++ 4_D_71220869.py
@@ -1,8 +1,8 @@
 a = input('Masukkan sekumpulan bilangan (pisahkan dengan koma): ')
 def bilangan(b):
     b = (a).split(',')
-    b.sort(key = lambda x: int(x))
+    b.sort(key = lambda abc: int(abc))
     return b
 c = bilangan(a)
-print(f'Bilangan terkecil dari kumpulan bilangan yang di input adalah {c[0]}')
 print(f'Bilangan terbesar dari kumpulan bilangan yang di input adalah {c[-1]}')
+print(f'Bilangan terkecil dari kumpulan bilangan yang di input adalah {c[0]}')
